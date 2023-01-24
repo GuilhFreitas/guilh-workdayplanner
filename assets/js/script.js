6cdcb942bd354af3a406177cc196c9291f3ecf68
GuillhFreitas <guilherme.antraco@gmail.com>
2023-01-24 22:07:20 +0000

    added functions to color code time blocks and update hourly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,47 @@
-let currentDayEl = $("#currentDay");
-let currentDay = moment().format("HH:mm:ss dddd, MMMM Do");
 let $window = $(window);
+let currentDayEl = $("#currentDay");
+let timeBlockEls = $("textarea");
 
+let startHour = Number(moment().format("H"));
+let currentDay = moment().format("dddd, MMMM Do");
 currentDayEl.text(currentDay);
 
+colorBlocks();
 updateDate();
+updateHour();
 
-// function updateDate(){
-//     let daily = setInterval(function(){
-//         currentDay = moment().format("dddd, MMMM Do")
-//         currentDayEl.text(currentDay);
-//     },1000)
-// }
-
+// updates date when window is focused on!
 function updateDate() {
     $window.on("focus", function(){
-        currentDay = moment().format("HH:mm:ss dddd, MMMM Do")
+        currentDay = moment().format("dddd, MMMM Do")
         currentDayEl.text(currentDay);
     })
+}
+
+// runs colorBlocks if the hour changes
+function updateHour() {
+    let timeInterval = setInterval(function() {
+        let newHour = Number(moment().format("H"));
+        if(newHour > startHour){
+            startHour = newHour;
+            colorBlocks();
+        }
+        
+    }, 1000)
+}
+
+// colors the time blocks depending on current time
+function colorBlocks() {
+    $.each(timeBlockEls, function(){
+        console.log('each running')
+        if ($(this).attr('data-hour') < startHour){
+            console.log('if running')
+            $(this).addClass('past');
+        }else if ($(this).attr('data-hour') == startHour){
+            console.log('elif running')
+            $(this).addClass('present');
+        }else{
+            console.log('else running')
+            $(this).addClass('future');
+        }})
 }
